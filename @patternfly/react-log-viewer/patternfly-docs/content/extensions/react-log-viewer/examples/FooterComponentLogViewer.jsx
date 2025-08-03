9c4abfb47e563aa38b433d2648177aea0c40745d
GitHub Workflow <cockpituous@cockpit-project.org>
2025-08-03 00:48:41 +0000

    cdeed08dccec5a16d64bf800849b9705ad3e5fd64fb0325445d29800ec0e1da3  package.json

---
+++ @patternfly/react-log-viewer/patternfly-docs/content/extensions/react-log-viewer/examples/FooterComponentLogViewer.jsx
@@ -0,0 +1,25 @@
+import { useRef } from 'react';
+import { data } from '@patternfly/react-log-viewer/patternfly-docs/content/extensions/react-log-viewer/examples/realTestData.js';
+import { LogViewer } from '@patternfly/react-log-viewer';
+import { Button } from '@patternfly/react-core';
+
+export const FooterComponentLogViewer = () => {
+  const logViewerRef = useRef(null);
+  const FooterButton = () => {
+    const handleClick = () => {
+      logViewerRef.current.scrollToBottom();
+    };
+    return <Button onClick={handleClick}>Jump to the bottom</Button>;
+  };
+
+  return (
+    <LogViewer
+      ref={logViewerRef}
+      hasLineNumbers={false}
+      height={300}
+      data={data.data}
+      theme="dark"
+      footer={<FooterButton />}
+    />
+  );
+};
